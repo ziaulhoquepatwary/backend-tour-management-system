5abc8eeb982ff719b8a16a221c362f9a17785b78
Ziaul Haque <ziaulhoque4820@gmail.com>
2026-04-28 19:31:26 +0600

    MongoDB connection success with project

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,22 @@
 import type { Server } from "http";
+import mongoose from "mongoose";
 import app from "./app.js";
 
 let server: Server;
 const PORT = 5000;
 
 const startServer = async () => {
-    server = app.listen(PORT, () => {
-        console.log(`Server running at http://localhost:${PORT}`);
-    })
+    try {
+        await mongoose.connect("mongodb+srv://tour-management:jIBaKPb6uD494iVB@cluster0.kogn06a.mongodb.net/tourManagementDB?appName=Cluster0");
+
+        console.log("Connnected to MongoDB using Mongoose!!");
+
+        server = app.listen(PORT, () => {
+            console.log(`Server running at http://localhost:${PORT}`);
+        })
+    } catch (error) {
+        console.log("MongoDB connection failed", error);
+    }
 }
 
 startServer();
