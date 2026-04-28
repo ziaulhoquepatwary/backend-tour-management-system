a045c98ed386aebc5e83529b9622b723f69d84d7
Ziaul Haque <ziaulhoque4820@gmail.com>
2026-04-28 21:37:57 +0600

    handle 3 types error (Unhandle Rejection, Uncaugth Exception, Sigterm signal)

--- src/server.ts
+++ src/server.ts
@@ -20,3 +20,46 @@ const startServer = async () => {
 }
 
 startServer();
+
+// unhandle rejection error:
+process.on("unhandledRejection", (err) => {
+    console.log("Undandle Rejection detected... Server shutting down..", err);
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+
+    process.exit(1)
+})
+
+// check unhandle rejection error:
+// Promise.reject(new Error("I forgot to catch this promise"))  
+
+process.on("uncaughtException", (err) => {
+    console.log("Uncaugth Exception detected... Server shutting down..", err);
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+
+    process.exit(1)
+})
+
+// check Uncaugth Exception error:
+// throw new Error ("I forgot to handle this local error"); 
+
+process.on("SIGTERM", () => {
+    console.log("Sigterm signal recieved... Server shutting down..");
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+
+    process.exit(1)
+})
