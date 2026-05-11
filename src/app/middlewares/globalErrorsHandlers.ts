4a182ef057de77ca7b1950dcc968623ae7e9ca91
Ziaul Haque <ziaulhoque4820@gmail.com>
2026-05-11 14:16:36 +0600

    Create custom Error class, AppErro

--- src/app/middlewares/globalErrorsHandlers.ts
+++ src/app/middlewares/globalErrorsHandlers.ts
@@ -2,11 +2,21 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type { NextFunction, Request, Response } from "express"
 import { envVars } from "../config/env.js"
+import AppError from "../errorHelpers/AppError.js";
 
 
 export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
     const statusCode = 500;
     const message = `Something went wrong!! ${err.message} from Global Error`
+
+
+    // if (err instanceof AppError) {
+    //     statusCode = err.statusCode
+    //     message = err.message
+    // } else if (err instanceof Error) {
+    //     statusCode = 500;
+    //     message = err.message
+    // }
 
     res.status(statusCode).json({
         success: false,
