7215677e3502993d5b36ed4771b595eefe26d0b2
Ziaul Haque <ziaulhoque4820@gmail.com>
2026-05-12 22:33:21 +0600

    Implement validateRequest Middleware

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,54 +1,12 @@
-import { Router, type NextFunction, type Request, type Response } from "express";
+import { Router } from "express";
 import { userControllers } from "./users.controllers.js";
-import z from "zod";
+import { createUserZodSchema } from "./user.validation.js";
+import { validateRequest } from "../../middlewares/validateRequest.js";
 
 
 const router = Router()
 
-router.post("/register",
-    async (req: Request, res: Response, next: NextFunction) => {
-
-        const createUserZodSchema = z.object({
-            name: z
-                .string({ message: "Name must be string" })
-                .min(3, { message: "Name must be at least 3 characters long" })
-                .max(50, { message: "Name cannot exceed 50 characters." }),
-            email: z
-                .string({ message: "Email must be string" })
-                .email({ message: "Invalid email address formate" })
-                .min(5, { message: "Email must be at least 5 characters long." })
-                .max(100, { message: "Email cannot exceed 100 characters." }),
-            password: z
-                .string({ message: "Password must be string" })
-                .min(8, { message: "Password must be at least 8 characters long." })
-                .regex(/^(?=.*[A-Z])/, {
-                    message: "Password must contain at least 1 uppercase letter.",
-                })
-                .regex(/^(?=.*[!@#$%^&*])/, {
-                    message: "Password must contain at least 1 special character.",
-                })
-                .regex(/^(?=.*\d)/, {
-                    message: "Password must contain at least 1 number.",
-                }),
-            phone: z
-                .string({ message: "Phone Number must be string" })
-                .regex(/^(?:\+8801\d{9}|01\d{9})$/, {
-                    message: "Phone number must be valid for Bangladesh. Format: +8801XXXXXXXXX or 01XXXXXXXXX",
-                })
-                .optional(),
-            address: z
-                .string({ message: "Address must be string" })
-                .max(200, { message: "Address cannot exceed 200 characters." })
-                .optional()
-        })
-
-        req.body = await createUserZodSchema.parseAsync(req.body)
-
-        console.log(req.body);
-
-        next()
-
-    }, userControllers.createUser)
+router.post("/register", validateRequest(createUserZodSchema), userControllers.createUser)
 router.get("/all-users", userControllers.getAllUsers)
 
 export const UserRoutes = router;
