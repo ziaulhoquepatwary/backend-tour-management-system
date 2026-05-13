619641ceabe14f67b2da35866d14fbb330ca2673
Ziaul Haque <ziaulhoque4820@gmail.com>
2026-05-13 23:28:08 +0600

    create an AccessToken during Login

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -3,6 +3,7 @@ import type { IUser } from "../user/user.interfaces.js";
 import httpStatus from "http-status-codes";
 import { User } from "../user/users.model.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const credentialsLogin = async (payload: Partial<IUser>) => {
     const { email, password } = payload;
@@ -19,8 +20,16 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
         throw new AppError(httpStatus.BAD_REQUEST, "Incorrect Password")
     }
 
+    const jwtPayload = {
+        userId: isUserExist._id,
+        email: isUserExist.email,
+        role: isUserExist.role
+    }
+
+    const accessToken = jwt.sign(jwtPayload, "sectet", { expiresIn: "1d" })
+
     return {
-        email: isUserExist.email
+        accessToken
     }
 }
 
