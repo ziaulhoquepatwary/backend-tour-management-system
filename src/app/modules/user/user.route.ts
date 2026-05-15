6b1cb49b2a36ef4578f6e798eb33a161683d5687
Ziaul Haque <ziaulhoque4820@gmail.com>
2026-05-15 20:23:03 +0600

    update user route

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { userControllers } from "./users.controllers.js";
-import { createUserZodSchema } from "./user.validation.js";
+import { createUserZodSchema, updateUserZodSchema } from "./user.validation.js";
 import { validateRequest } from "../../middlewares/validateRequest.js";
 import { Role } from "./user.interfaces.js";
 import { checkAuth } from "../../middlewares/checkAuth.js";
@@ -10,6 +10,6 @@ const router = Router()
 
 router.post("/register", validateRequest(createUserZodSchema), userControllers.createUser)
 router.get("/all-users", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), userControllers.getAllUsers)
-router.patch("/:id", checkAuth(...Object.values(Role)), userControllers.updateUser)
+router.patch("/:id", validateRequest(updateUserZodSchema), checkAuth(...Object.values(Role)), userControllers.updateUser)
 
 export const UserRoutes = router;
