acc8f9e3cb86278b6a7134504356caff8c1685cb
Ziaul Haque <ziaulhoque4820@gmail.com>
2026-05-16 20:54:36 +0600

    separate user token functionlity and Create createUserTokens function.

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -6,6 +6,7 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { generateToken } from "../../utils/jwt.js";
 import { envVars } from "../../config/env.js";
+import { createUserTokens } from "../../utils/userTokens.js";
 
 const credentialsLogin = async (payload: Partial<IUser>) => {
     const { email, password } = payload;
@@ -22,23 +23,14 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
         throw new AppError(httpStatus.BAD_REQUEST, "Incorrect Password")
     }
 
-    const jwtPayload = {
-        userId: isUserExist._id,
-        email: isUserExist.email,
-        role: isUserExist.role
-    }
-
-    // const accessToken = jwt.sign(jwtPayload, "sectet", { expiresIn: "1d" })
-    const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES)
-
-    const refreshToken = generateToken(jwtPayload, envVars.JWT_REFRESH_SECRET, envVars.JWT_REFRESH_EXPIRES)
+    const userTokens = createUserTokens(isUserExist)
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { password: pass, ...rest } = isUserExist;
+    const { password: pass, ...rest } = isUserExist.toObject();
 
     return {
-        accessToken,
-        refreshToken,
+        accessToken: userTokens.accessToken,
+        refreshToken: userTokens.refreshToken,
         user: rest
     }
 }
